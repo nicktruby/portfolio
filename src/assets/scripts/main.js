8d16f45b88306c633515b2793838915963ba2dac
Nick Truby <truby@me.com>
2020-11-09 12:12:18 +0000

    add navOpenClose() to the nav links

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,12 +1,8 @@
-// <--- To open the mobile nav menu --- > //
-
 const navOpenClose = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav');
+  const navLinks = document.querySelectorAll('.nav-link');
   burger.addEventListener('click', () => nav.classList.toggle('open'));
+  navLinks.forEach(link => link.addEventListener('click', () => nav.classList.toggle('open')));
 }
 navOpenClose();
-
-// <--- Intersection Observers to flip styles on the navbar --- > //
-
-  
